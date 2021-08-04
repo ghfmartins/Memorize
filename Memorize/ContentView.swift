9111b3f9f24201ad8c6fa17333eb94d8425b7377
Gustavo Martins <gh.martins9@gmail.com>
2021-08-04 00:14:34 -0300

    First half of the lecture

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,42 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
-    var emojis = ["🚌","🏎","🚀","🚅","🚜","🛵","🚁","🛳","🚗","🛻","🚲","🛸","🚚"]
-    @State var emojiCount = 12
+    @ObservedObject var viewModel: EmojiMemoryGame
     
     var body: some View {
-        VStack {
-            ScrollView {
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
-                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
-                        CardView(content: emoji)
-                            .aspectRatio(2/3, contentMode: .fit)
-                    }
+        ScrollView {
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                ForEach(viewModel.cards) { card in
+                    CardView(card: card)
+                        .aspectRatio(2/3, contentMode: .fit)
+                        .onTapGesture {
+                            viewModel.choose(card)
+                        }
                 }
             }
-            .foregroundColor(.red)
         }
+        .foregroundColor(.red)
         .padding(.horizontal)
     }
 }
 
 struct CardView: View{
-    var content: String
-    @State var isFaceUp: Bool = true
-    
+    let card: MemoryGame<String>.Card
     var body: some View {
         ZStack {
             let shape = RoundedRectangle(cornerRadius: 20)
-            if isFaceUp {
+            if card.isFaceUp {
                 shape.fill().foregroundColor(.white)
                 shape.strokeBorder(lineWidth: 3)
-                Text(content).font(.largeTitle)
+                Text(card.content).font(.largeTitle)
             } else {
                 shape.fill()
             }
-        }
-        .onTapGesture {
-            isFaceUp = !isFaceUp
         }
     }
 }
@@ -70,12 +65,10 @@ struct CardView: View{
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        Group {
-            ContentView()
-                .preferredColorScheme(.light)
-            ContentView()
-                .preferredColorScheme(.dark)
-        }
-            
+        let game = EmojiMemoryGame()
+        ContentView(viewModel: game)
+            .preferredColorScheme(.light)
+        ContentView(viewModel: game)
+            .preferredColorScheme(.dark)
     }
 }
--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -7,8 +7,7 @@
 
 import SwiftUI
 
-class EmojiMemoryGame {
-    
+class EmojiMemoryGame: ObservableObject {
     static let emojis = ["🚌","🏎","🚀","🚅","🚜","🛵","🚁","🛳","🚗","🛻","🚲","🛸","🚚"]
     
     static func createMemoryGame() -> MemoryGame<String> {
@@ -17,12 +16,18 @@ class EmojiMemoryGame {
         }
     }
     
-    private var model: MemoryGame<String> = createMemoryGame()
+    @Published private var model: MemoryGame<String> = createMemoryGame()
         
     
     // Essa é uma outra forma de acessar algum elemento do Model sem pertimir que ele seja modificado.
     var cards: Array<MemoryGame<String>.Card> {
-        return model.cards
+        model.cards
     }
 
+    // MARK: - Intent(s)
+    
+    func choose(_ card: MemoryGame<String>.Card) {
+        //objectWillChange.send() -> uma possibilidade, mas usaremos o @published junto a declaração do model.
+        model.choose(card)
+    }
 }
